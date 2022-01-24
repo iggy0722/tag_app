5b0f309231390135268d417be5a1317f0026f87f
iggy0722 <iggy.mustbenice.0722@gmail.com>
2022-01-24 11:12:58 +0900

    バリデーションの処理

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -1,3 +1,11 @@
 class PostForm
   include ActiveModel::Model
+
+  #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
+  attr_accessor :text, :image
+
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
 end
